395328712e3da88ce30fce581a329163a409de8c
emilhong <58834792+emilhong@users.noreply.github.com>
2021-10-04 08:34:39 +0800

    Update config.py

--- api/app/config.py
+++ api/app/config.py
@@ -1,15 +1,1 @@
-import enum
-
-URL_PREFIX = '/qaqc/api'
-
-# class MRBActionEnum(enum.IntEnum):
-#     NONE = 1
-#     CREATE_EDIT = 2
-#     CREATE_EDIT_DELETE = 3
-#     ALL_APPROVAL = 4
-
-# class QAQCActionEnum(enum.IntEnum):
-#     ALL = 1
-#     CREATE = 2
-#     EDIT = 3
-#     DELETE = 4
+URL_PREFIX = '/assessment/api'
